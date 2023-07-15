ce607f94a2ddec07d7ace42a889adf78374ceafd
TretyakWeb <tretyakbro@gmail.com>
2023-07-16 02:40:31 +0300

    feat: add navigation and screens
    This commit adds navigation functionality and two screens: CategoriesScreen and MealsOverviewScreen. The CategoriesScreen displays a list of categories, and the MealsOverviewScreen shows a basic view with the text "Meals Overview Screen". The necessary dependencies and fonts are also loaded in the App component.

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Core
+import { useEffect } from "react";
+import { StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// Screens
+import { CategoriesScreen, MealsOverviewScreen } from "./screens";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({
+    "roboto-300": require("./assets/fonts/Roboto-Light.ttf"),
+    "roboto-400": require("./assets/fonts/Roboto-Regular.ttf"),
+    "roboto-700": require("./assets/fonts/Roboto-Bold.ttf"),
+    "gilroy-800": require("./assets/fonts/Gilroy-Extrabold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
