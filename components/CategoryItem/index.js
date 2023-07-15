3c852b600de96ac5ae1a73828d71b736823612c9
TretyakWeb <tretyakbro@gmail.com>
2023-07-16 02:35:09 +0300

    feat: add CategoryItem component
    This commit adds the CategoryItem component, which represents an item in a category grid. The CategoryItem component displays a title and responds to onPress events. It has a dynamic background color based on the provided color prop. The component is styled using the StyleSheet module from react-native.
    
    The CategoryItem component is used to render items in a category grid, providing a visually appealing and interactive interface for selecting categories.

---
+++ components/CategoryItem/index.js
@@ -0,0 +1,54 @@
+// Core
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import { colors } from "../../constants";
+
+export const CategoryItem = ({ title, color, onPress }) => {
+  return (
+    <View style={[styles.gridItem, { backgroundColor: color }]}>
+      <Pressable
+        android_ripple={{ color: colors.white }}
+        style={({ pressed }) =>
+          pressed ? [styles.button, styles.buttonPressed] : styles.button
+        }
+        onPress={onPress}
+      >
+        <View style={styles.innerContainer}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+    elevation: 4,
+    borderRadius: 10,
+    backgroundColor: colors.white,
+    shadowColor: colors.black,
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: "hidden",
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.25,
+    backgroundColor: colors.white,
+  },
+  title: {
+    fontFamily: "gilroy-800",
+    fontSize: 16,
+  },
+});
