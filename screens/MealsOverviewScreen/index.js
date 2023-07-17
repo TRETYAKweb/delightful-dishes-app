013db992c1a63a412a91030756cb9c89fdcc4eba
TretyakWeb <tretyakbro@gmail.com>
2023-07-17 04:55:29 +0300

    feat: display meals based on category
    This commit updates the MealsOverviewScreen component to display meals based on the selected category. The component receives the categoryId as a route parameter and filters the MEALS data accordingly. The filtered meals are rendered using the MealItem component in a FlatList.

--- screens/MealsOverviewScreen/index.js
+++ screens/MealsOverviewScreen/index.js
@@ -1,9 +1,47 @@
-import { View, Text } from "react-native";
+// Core
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { useRoute } from "@react-navigation/native";
+
+// Components
+import { MealItem } from "../../components";
+
+// Data
+import { MEALS } from "../../data/dummy-data";
 
 export const MealsOverviewScreen = () => {
+  const route = useRoute();
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((item) =>
+    item.categoryIds.includes(catId)
+  );
+
+  const renderMealItem = ({ item }) => {
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+
+    return <MealItem {...mealItemProps} />;
+  };
+
   return (
-    <View>
-      <Text>Meals Overview Screen</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
