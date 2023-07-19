68e4fa8c927042b4c948676531d8b68a01dcc921
TretyakWeb <tretyakbro@gmail.com>
2023-07-19 03:00:45 +0300

    feat: Add meal details screen and update meal details component

--- screens/MealDetailsScreen/index.js
+++ screens/MealDetailsScreen/index.js
@@ -1,38 +1,55 @@
 // Core
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 import { useRoute } from "@react-navigation/native";
 
 // Components
-import { MealDetails } from "../../components/MealDetails";
+import { MealDetails, Subtitle, List } from "../../components";
 
 // Data
 import { MEALS } from "../../data/dummy-data";
+import { colors } from "../../constants";
 
 export const MealDetailsScreen = () => {
   const mealId = useRoute().params.mealId;
   const selectedMeal = MEALS.find((item) => item.id === mealId);
 
-  const ingredientsJSX = selectedMeal.ingredients.map((ingredient) => {
-    return <Text key={ingredient}>{ingredient}</Text>;
-  });
-
-  const stepsJSX = selectedMeal.ingredients.map((step) => {
-    return <Text key={step}>{step}</Text>;
-  });
-
   return (
-    <View>
-      <Image source={{ uri: selectedMeal.imageUrl }} />
-      <Text>{selectedMeal.title}</Text>
-      <MealDetails
-        duration={selectedMeal.duration}
-        complexity={selectedMeal.complexity}
-        affordability={selectedMeal.affordability}
-      />
-      <Text>Ingredients</Text>
-      <View>{ingredientsJSX}</View>
-      <Text>Steps</Text>
-      <View>{stepsJSX}</View>
-    </View>
+    <ScrollView style={{ marginBottom: 15 }}>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <View style={styles.mainContainer}>
+        <Text style={styles.title}>{selectedMeal.title}</Text>
+        <MealDetails
+          duration={selectedMeal.duration}
+          complexity={selectedMeal.complexity}
+          affordability={selectedMeal.affordability}
+          style={styles.detailsContainer}
+        />
+        <Subtitle>Ingredients</Subtitle>
+        <View style={{ marginBottom: 35 }}>
+          <List data={selectedMeal.ingredients} />
+        </View>
+        <Subtitle>Steps</Subtitle>
+        <List data={selectedMeal.steps} />
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  mainContainer: {
+    padding: 25,
+  },
+  detailsContainer: {
+    marginHorizontal: 0,
+  },
+  title: {
+    fontFamily: "gilroy-800",
+    marginBottom: 15,
+    fontSize: 24,
+    color: colors.white,
+  },
+});
