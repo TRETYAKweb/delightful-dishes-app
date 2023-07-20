57c890a79af25e1015d4048a2f8e88ddf8cff5ba
TretyakWeb <tretyakbro@gmail.com>
2023-07-20 05:59:56 +0300

    diff --git a/components/IconButton/index.js b/components/IconButton/index.js new file mode 100644 index 0000000..d34eba2 --- /dev/null +++ b/components/IconButton/index.js @@ -0,0 +1,19 @@ +import { Pressable, StyleSheet } from "react-native"; +import { Ionicons } from "@expo/vector-icons"; + +export const IconButton = ({ name, color, onPress }) => { +  return ( +    <Pressable +      style={({ pressed }) => pressed && styles.pressed} +      onPress={onPress} +    > +      <Ionicons name={name} size={24} color={color} /> +    </Pressable> +  ); +}; + +const styles = StyleSheet.create({ +  pressed: { +    opacity: 0.7, +  }, +});

---
+++ components/IconButton/index.js
@@ -0,0 +1,19 @@
+import { Pressable, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export const IconButton = ({ name, color, onPress }) => {
+  return (
+    <Pressable
+      style={({ pressed }) => pressed && styles.pressed}
+      onPress={onPress}
+    >
+      <Ionicons name={name} size={24} color={color} />
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.7,
+  },
+});
