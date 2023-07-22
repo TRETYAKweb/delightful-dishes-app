80ff25bd29cdab57dbbaf612774302b0368b179e
TretyakWeb <tretyakbro@gmail.com>
2023-07-23 02:33:09 +0300

    feat(navigation): add drawer navigation
    Update the 'App.js' file to include a DrawerNavigator for improved navigation in the application. The DrawerNavigator contains two screens: 'MealCategories' and 'Favorites', accessible through a drawer menu. The 'MealCategories' screen displays all meal categories, and the 'Favorites' screen displays favorite items.

--- App.js
+++ App.js
@@ -5,16 +5,62 @@ import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
 
 // Screens
 import {
   CategoriesScreen,
   MealsOverviewScreen,
   MealDetailsScreen,
+  FavoritesScreen,
 } from "./screens";
 import { colors } from "./constants";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigation = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#36273C",
+        },
+        headerTintColor: colors.white,
+        sceneContainerStyle: {
+          backgroundColor: "#503E57",
+        },
+        drawerContentStyle: {
+          backgroundColor: "#36273C",
+        },
+        drawerActiveBackgroundColor: "#503E57",
+        drawerActiveTintColor: colors.accent300,
+        drawerInactiveTintColor: colors.white,
+      }}
+    >
+      <Drawer.Screen
+        name="MealCategories"
+        component={CategoriesScreen}
+        options={{
+          headerTitle: "All Categories",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
 
 export default function App() {
   SplashScreen.preventAutoHideAsync();
@@ -55,10 +101,10 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigation}
             options={{
-              title: "All Categories",
+              headerShown: false,
             }}
           />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
