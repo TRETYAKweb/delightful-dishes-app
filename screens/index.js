4bb252686d5775880b9892febdc288ca6b5a070f
TretyakWeb <tretyakbro@gmail.com>
2023-07-23 02:31:54 +0300

    chore(screens): add FavoritesScreen export
    Update the 'screens/index.js' file to export the FavoritesScreen component, allowing other parts of the application to import and use it.

--- screens/index.js
+++ screens/index.js
@@ -1,3 +1,4 @@
 export { CategoriesScreen } from "./CategoriesScreen";
 export { MealsOverviewScreen } from "./MealsOverviewScreen";
 export { MealDetailsScreen } from "./MealDetailsScreen";
+export { FavoritesScreen } from "./FavoritesScreen";
