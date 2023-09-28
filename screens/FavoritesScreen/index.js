dd586a22b94f96bb9eeca1374d2545f9cece871e
TretyakWeb <tretyakbro@gmail.com>
2023-09-29 00:05:50 +0300

    feat(FavoritesScreen): implement displaying favorite meals

--- screens/FavoritesScreen/index.js
+++ screens/FavoritesScreen/index.js
@@ -1,9 +1,48 @@
-import { Text, View } from "react-native";
+import { useContext } from "react";
+import { FavoritesContext } from "../../lib/context/favorites-context";
+import { MEALS } from "../../data/dummy-data";
+import { FlatList } from "react-native-gesture-handler";
+import { MealItem } from "../../components";
+import { StyleSheet, Text, View } from "react-native";
+
 
 export const FavoritesScreen = () => {
-  return (
-    <View>
-      <Text>FavoritesScreen</Text>
-    </View>
-  );
+
+  const favoritesMealIds = useContext(FavoritesContext);
+  const filteredMeals = MEALS.filter(it => favoritesMealIds?.ids.includes(it.id));
+
+  const renderItem = ({item}) => {
+
+    const mealItemProps = {
+      id: item.id,
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+
+    return <MealItem {...mealItemProps} />
+  }
+
+  if(favoritesMealIds.ids.length === 0) {
+    return <View style={styles.root}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+  }
+
+  return <FlatList data={filteredMeals} keyExtractor={item => item.id} renderItem={renderItem} />
 };
+
+const styles = StyleSheet.create({
+  root: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#fff'
+  }
+})
