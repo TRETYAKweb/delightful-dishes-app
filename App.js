41c88d6e0e47e250d0a75cade5781ac8ffb4178d
TretyakWeb <tretyakbro@gmail.com>
2023-09-29 00:09:00 +0300

    feat(App): integrate navigation and context providers

--- App.js
+++ App.js
@@ -3,64 +3,10 @@ import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { Ionicons } from "@expo/vector-icons";
 
-// Screens
-import {
-  CategoriesScreen,
-  MealsOverviewScreen,
-  MealDetailsScreen,
-  FavoritesScreen,
-} from "./screens";
-import { colors } from "./constants";
-
-const Stack = createNativeStackNavigator();
-const Drawer = createDrawerNavigator();
-
-const DrawerNavigation = () => {
-  return (
-    <Drawer.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#36273C",
-        },
-        headerTintColor: colors.white,
-        sceneContainerStyle: {
-          backgroundColor: "#503E57",
-        },
-        drawerContentStyle: {
-          backgroundColor: "#36273C",
-        },
-        drawerActiveBackgroundColor: "#503E57",
-        drawerActiveTintColor: colors.accent300,
-        drawerInactiveTintColor: colors.white,
-      }}
-    >
-      <Drawer.Screen
-        name="MealCategories"
-        component={CategoriesScreen}
-        options={{
-          headerTitle: "All Categories",
-          drawerIcon: ({ color, size }) => (
-            <Ionicons name="list" color={color} size={size} />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="Favorites"
-        component={FavoritesScreen}
-        options={{
-          drawerIcon: ({ color, size }) => (
-            <Ionicons name="heart" color={color} size={size} />
-          ),
-        }}
-      />
-    </Drawer.Navigator>
-  );
-};
+// lib
+import { Navigation } from "./lib/Navigation";
+import { FavoritesContextProvider } from "./lib/context/favorites-context";
 
 export default function App() {
   SplashScreen.preventAutoHideAsync();
@@ -84,36 +30,9 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: "#36273C",
-            },
-            headerTintColor: colors.white,
-            contentStyle: {
-              backgroundColor: "#503E57",
-            },
-            headerBackTitle: "Back",
-            headerBackTitleStyle: {
-              fontFamily: "roboto-700",
-            },
-          }}
-        >
-          <Stack.Screen
-            name="Drawer"
-            component={DrawerNavigation}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-          <Stack.Screen
-            name="MealDetailsScreen"
-            component={MealDetailsScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <FavoritesContextProvider>
+        <Navigation />
+      </FavoritesContextProvider>
     </>
   );
 }
